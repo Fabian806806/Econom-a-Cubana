ab6f5334ad61a489df4c9a38fbdb9c692ee1f19b
enriquefabio785-sudo <enriquefabio785@gmail.com>
2025-12-08 21:04:53 +0100

    avanzando funciones

--- funciones.py
+++ funciones.py
@@ -4,8 +4,11 @@ import json
 with open("E:/Economía Cuba/data/PRODUCTOS.json","r",encoding="utf-8") as f:
     Productos=json.load(f)
 
-with open("E:/Economía Cuba/data/El_Toque/USD_Noviembre.json",encoding="utf-8") as f:
+with open("E:/Economía Cuba/data/El_Toque/USD_Noviembre.json","r",encoding="utf-8") as f:
     Toque=json.load(f)
+    
+with open("E:/Economía Cuba/data/Salario/Salario_Mensual_Estatal.json","r",encoding="utf-8") as f:
+    Salarios=json.load(f)
 
 
 def medianaP(P000,tipo="precio"):
@@ -63,8 +66,64 @@ def mediana_varios(MP000,tipo="precio"):
     return Medianas
 
 
-            
 
 
 
+
+def maxmin(data):
+    min=max=None
+    
+    if data==Salarios:
+        for x in data["Salarios por actividad económica"]:
+            if min==None:
+                min=max=data["Salarios por actividad económica"][x]
         
+            elif data["Salarios por actividad económica"][x]<min:
+                min=data["Salarios por actividad económica"][x]
+        
+            elif data["Salarios por actividad económica"][x]>max:
+                max=data["Salarios por actividad económica"][x]
+    
+    if data==Toque:
+        for x in data:
+            if min==None:
+                min=max=x["valor"]
+            
+            elif x["valor"]<min:
+                min=x["valor"]
+            
+            elif x["valor"]>max:
+                max=x["valor"]
+    
+    return f"{min} CUP , {max} CUP"
+            
+       
+def promed(data):
+    suma=0
+    cant=0
+    
+    if data==Salarios:
+        for x in data["Salarios por actividad económica"]:
+            suma+=data["Salarios por actividad económica"][x]
+            cant+=1
+    
+    if data==Toque:
+        for x in data:
+            suma+=x["valor"]
+            cant+=1
+  
+    return round(suma/cant,1)
+
+           
+
+
+
+
+
+def salario_to_USD(sector):
+    conversión=round(Salarios["Salarios por actividad económica"][sector]/promed(Toque),1)
+    
+    return f"{conversión} USD"
+
+
+      
